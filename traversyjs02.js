6a9b27f2a0caf31d6c8bbd49cb895101f979a1f7
spaceC00kie <kirstie317@gmail.com>
2020-12-01 00:40:56 -0500

    console.log and const

---
+++ traversyjs02.js
@@ -0,0 +1,33 @@
+//console.log("Hello World");
+//console.error("This is an error");
+//console.warn("This is a warning");
+
+// let, const
+
+//let age = 30;
+//age = 31;
+//console.log(age);
+
+//let score;
+//score = 18;
+//console.log(score);
+
+// String, Numbers, Boolean, Null, Undefined
+
+const name = 'John';
+const age = 23;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof isCool);
+console.log(typeof x);
+//type of x shows up as object in console - this is wrong
+
+console.log(typeof y);
+console.log(typeof z);
